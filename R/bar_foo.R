af47909b65c485555b0fbc0939757266a0317d63
Timofei Shashkov <timofei98shashkov@gmail.com>
2025-11-14 10:48:30 +0100

    Updated documentation*

--- R/bar_foo.R
+++ R/bar_foo.R
@@ -1,6 +1,7 @@
 #' bar method for foo objects
 #'
 #' @param object A foo object
+#' @param ... Some additional paramteres (ignored)
 #' @export
 bar.foo <- function(object, ...) {
   # do whatever your method should do
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-
+git
 # OOPPackage
 
 <!-- badges: start -->
