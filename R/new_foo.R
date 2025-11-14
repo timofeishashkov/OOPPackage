83e4bf7a81871494e578a5bfdfc3249cd364908f
Timofei Shashkov <timofei98shashkov@gmail.com>
2025-11-14 15:01:05 +0100

    Put class + methdo in the same R file

--- R/new_foo.R
+++ R/new_foo.R
@@ -11,3 +11,27 @@ new_foo <- function(x = numeric()) {
     class = "foo"
   )
 }
+
+#' Generic function bar
+#'
+#' @param object An object
+#' @param ... Some additional parameters (ignored)
+#'
+#' @return Method depends on class
+#' @export
+bar <- function(object, ...) {
+  UseMethod("bar")
+}
+
+#' bar method for foo objects
+#'
+#' @param object A foo object
+#' @param ... Some additional paramteres (ignored)
+#'
+#' @export
+bar.foo <- function(object, ...) {
+  # do whatever your method should do
+  mean(object$x)
+}
+
+
